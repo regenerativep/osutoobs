5b2ca1719563a3c75b876ae5f2ab6fac3b311f30
regenerativep <regeneralp@gmail.com>
2018-02-21 13:31:09 -0500

    structured things a bit better; panel stuff

--- osutoobs/index.js
+++ osutoobs/index.js
@@ -1,102 +1,153 @@
-"use strict"
+"use strict";
 const CP = require("child_process");
 const HTTP = require("http");
 const URL = require("url");
 const FS = require("fs");
-const SocketIo = require("socket.io")(HTTP);
+const WS = require("ws");
 
-var HttpServer = HTTP.createServer(function(req, res)
+var webManager;
+
+var WebManager = (function()
 {
-	var parsedUrl = URL.parse(req.url);
-	console.log(parsedUrl);
-	var actualFile = GetContent(parsedUrl.pathname);
-	FS.readFile(".\\" + actualFile, function(err, data)
+	function WebManager()
 	{
-		if(err)
+		this.StartWebServer();
+		this.StartTitleServer();
+		this.StartPanelServer();
+	}
+	WebManager.prototype.StartWebServer = function()
+	{
+		var gc = this.GetContent, gm = this.GetMimeType;
+		this.WebServer = HTTP.createServer(function(req, res)
 		{
-			console.log("failed to read " + actualFile);
-			return;
-		}
-		res.writeHead(200, {"Content-Type": GetMimeType(actualFile)});
-		//res.write(data);
-		res.end(data, "utf8", function()
-		{
-			console.log("sent");
+			var parsedUrl = URL.parse(req.url);
+			console.log(parsedUrl);
+			var actualFile = gc(parsedUrl.pathname);
+			FS.readFile(".\\" + actualFile, function(err, data)
+			{
+				if(err)
+				{
+					console.log("failed to read " + actualFile);
+					return;
+				}
+				res.writeHead(200, {"Content-Type": gm(actualFile)});
+				//res.write(data);
+				res.end(data, "utf8", function()
+				{
+					console.log("sent");
+				});
+			});
 		});
-	});
-});
-
-var GetContent = function(address)
-{
-	if(address.charAt(0) === "/")
-		address = address.substring(1);
-	var parts = address.split("/");
-	switch(parts[0])
+		this.WebServer.listen(80);
+	};
+	WebManager.prototype.StartTitleServer = function()
 	{
-		case "index.html":
-			return "index.html";
-		case "favicon.ico":
-			return "favicon.ico";
-		case "socket.io.js":
-			return "socket.io.js";
-		case "socket.io.js.map":
-			return "socket.io.js.map";
-		case "socket.io.slim.js":
-			return "socket.io.slim.js";
-		case "socket.io.slim.js.map":
-			return "socket.io.slim.js.map";
-	}
-	return "404.html";
-};
-var GetMimeType = function(name)
-{
-	var ind = name.lastIndexOf(".");
-	if(ind < 0)
+		this.TitleServer = new WS.Server({ port: 5525 });
+		this.TitleServer.on("connection", function connection(ws)
+		{
+			var tm;
+			ws.on("message", function incoming(message)
+			{
+				//we shouldnt actually be receiving any messages
+			});
+			ws.on("close", function incoming(c, d)
+			{
+				console.log("title client disconnect");
+				clearInterval(tm);
+			});
+			ws.on("error", function error(e)
+			{
+				console.log(e);
+			});
+			tm = setInterval(function()
+			{
+				GetOsuTitle(function(title)
+				{
+					if(ws.readyState == 1)
+						ws.send(title);
+				});
+			}, 2000);
+		});
+	};
+	WebManager.prototype.StartPanelServer = function()
+	{
+		this.PanelServer = new WS.Server({ port: 5524 });
+		this.PanelServer.on("connection", function connection(ws)
+		{
+			ws.on("message", function incoming(message)
+			{
+				console.log(message);
+			});
+			ws.on("close", function incoming(c, d)
+			{
+				console.log("disconnected");
+			});
+			ws.on("error", function error(e)
+			{
+				console.log(e);
+			});
+			ws.send("hey");
+		});
+	};
+	WebManager.prototype.GetContent = function(address)
+	{
+		if(address.charAt(0) === "/")
+			address = address.substring(1);
+		var parts = address.split("/");
+		switch(parts[0])
+		{
+			case "index.html":
+				return "index.html";
+			case "title.html":
+				return "title.html";
+			case "favicon.ico":
+				return "favicon.ico";
+		}
+		return "404.html";
+	};
+	WebManager.prototype.GetMimeType = function(name)
+	{
+		var ind = name.lastIndexOf(".");
+		if(ind < 0)
+			return "text/plain";
+		var ending = name.substring(ind);
+		switch(ending)
+		{
+			case ".html":
+				return "text/html";
+			case ".ico":
+				return "image/x-icon";
+			case ".map":
+				return "application/x-navimap";
+		}
 		return "text/plain";
-	var ending = name.substring(ind);
-	switch(ending)
-	{
-		case ".html":
-			return "text/html";
-		case ".ico":
-			return "image/x-icon";
-		case ".map":
-			return "application/x-navimap";
-	}
-	return "text/plain";
-};
+	};
+	return WebManager;
+}());
 
+var offlineMessage = "osu! is currently offline";
 var GetOsuTitle = function(cb)
 {
-	var title = "";
+	var title = offlineMessage;
 	CP.exec("tasklist /fi \"IMAGENAME eq osu!.exe\" /v /fo CSV /nh", function(err, stdout, stderr)
 	{
 		var lines = stdout.toString().split("\n");
 		var parts = lines[0].split(",");
 		//"Image Name","PID","Session Name","Session#","Mem Usage","Status","User Name","CPU Time","Window Title"
-		if(parts[0] != "\"osu!.exe\"")
+		if(parts.length < 9 || parts[0] != "\"osu!.exe\"")
 		{
-			console.log("unable to find osu process");
+			//console.log("unable to find osu process");
+			cb(offlineMessage);
 			return;
 		}
 		var txt = parts[9].substring(1, parts[9].length - 2);
 		title = txt;
+		if(title === "__wglDummyWindowFodder")
+		{
+			title = offlineMessage;
+		}
 		cb(title);
 	});
 };
 
-HttpServer.listen(80);
-SocketIo.on("connection", function()
-{
-	console.log("ey");
-	socket.emit("news", { hello: "world" });
-});
-
-
-/*var tm = setInterval(function()
-{
-	GetOsuTitle(function(title)
-	{
-		//console.log(title);
-	});
-}, 2000);*/
+webManager = new WebManager();
